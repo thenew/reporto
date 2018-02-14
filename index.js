436a702c1b5189ef65cb688a65cfeddc272289f2
thenew <remy@thenew.fr>
2018-02-14 17:36:45 +0100

    settings

--- index.js
+++ index.js
@@ -1,58 +1,43 @@
 const puppeteer = require('puppeteer')
 const mkdirp = require('mkdirp')
 const argv = require('minimist')(process.argv.slice(2))
+const SETTINGS = require('./settings')
 
 async function run() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
-  const urls = {
-    'HP': 'http://localhost:8080/',
-    'PDP': 'http://localhost:8080/eqt-support-adv-shoes/BB1305.html',
-  }
-
-  const viewports = {
-    'desktop': {
-      width: 1425,
-      height: 804
-    },
-    'mobile': {
-      width: 375,
-      height: 1500,
-      deviceScaleFactor: 2,
-      isMobile: true
-    },
-  }
-
-  const today = new Date();
+  const today = new Date()
   let directoryName = today.getFullYear()
   +('0' + (today.getMonth() + 1)).slice(-2)
   +('0' + (today.getDate() + 1)).slice(-2)
   + '-'
   +('0' + (today.getHours() + 1)).slice(-2)
   +('0' + (today.getMinutes() + 1)).slice(-2)
-  +('0' + (today.getSeconds() + 1)).slice(-2)
 
-  if(argv.tag) {
-    directoryName += '-'+argv.tag
-  }
+  for (var pageName in SETTINGS.urls) {
 
-  for (var pageName in urls) {
+    for (var viewport in SETTINGS.viewports) {
 
-    for (var viewport in viewports) {
-      await page.setViewport(viewports[viewport])
-      await page.goto(urls[pageName])
-      await page.waitFor(3000)
+      await page.setViewport(SETTINGS.viewports[viewport])
+      await page.goto(SETTINGS.urls[pageName])
+      // await page.waitFor(3000)
       await mkdirp('screenshots/'+directoryName)
-      // await mkdirp('/screenshots/'+directoryName)
 
-      // mkdirp('/screenshots/'+directoryName, (err) => {
-        // console.log('err: ', err)
-        await page.screenshot({path: 'screenshots/'+directoryName+'/'+pageName+'_'+viewport+'.png', fullPage: true});
-        console.log('screenshots/'+pageName+'_'+viewport+'.png');
-      // })
+      let fileName = pageName+'-'+viewport
+      if(argv.tag) {
+        fileName += '-'+argv.tag
+      }
+
+      fileName += '.png'
+
+      await page.screenshot({path: 'screenshots/'+directoryName+'/'+fileName, fullPage: true})
+      console.log(fileName+' saved')
     }
   }
+
+  console.log('Reporto done (ᵔᴥᵔ)')
+  process.exit(-1)
 }
 
 run()
---
+++ settings_example.js
@@ -0,0 +1,17 @@
+module.exports = {
+  urls: {
+    'homepage': 'http://github.com/',
+  },
+  viewports: {
+    'desktop': {
+      width: 1425,
+      height: 804
+    },
+    'mobile': {
+      width: 375,
+      height: 1500,
+      deviceScaleFactor: 2,
+      isMobile: true
+    },
+  }
+}
